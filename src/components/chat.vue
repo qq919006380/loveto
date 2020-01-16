e5c8e8d5c9029425bab15ef7681a7c7c7183a9d9
weibo <919006380@qq.com>
2020-01-17 00:47:08 +0800

    update

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -49,9 +49,9 @@ var host;
 if (process.env.NODE_ENV === "development") {
   host = "http://localhost:8080";
 } else if (process.env.NODE_ENV === "production") {
-  host = "http://47.244.164.231:8080";
-  // host = "http://192.168.31.168:3000";
-  // host = "http://localhost:8080";
+  host = "http://47.244.164.231:3000";//服务器
+  // host = "http://192.168.31.168:8080";//本地1
+  // host = "http://localhost:8080";//本地2
 }
 const socket = io(host);
 console.log(process.env.NODE_ENV);
